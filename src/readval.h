3cf02f5f6f197005bce3496490e507c660a42ba4
Olly Betts <olly@survex.com>
1998-03-21 15:09:27 +0000

    removed TRIM_PREFIX
    git-svn-id: file:///home/survex-svn/survex/trunk@8 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/readval.h
+++ src/readval.h
@@ -1,15 +1,14 @@
 /* > readval.h
  * Header file for routines to read a datum from the current input file
- * Copyright (C) 1991-1994 Olly Betts
+ * Copyright (C) 1991-1994,1997 Olly Betts
  */
 
 /*
 1994.04.16 created
 1994.05.09 moved TRIM_PREFIX from readval.c
 1994.11.13 removed read_numeric_UD and fDefault argument from read_numeric
+1997.08.24 removed TRIM_PREFIX
 */
-
-#define TRIM_PREFIX
 
 extern prefix *read_prefix( bool fOmit );
 extern real    read_numeric( bool fOmit );
