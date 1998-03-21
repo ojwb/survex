1833e3b591e3533efb18c070e64ce1045accf055
Olly Betts <olly@survex.com>
1998-03-21 15:13:35 +0000

    converted to cvrotgfx fettled
    git-svn-id: file:///home/survex-svn/survex/trunk@17 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/labels.c
+++ src/labels.c
@@ -14,15 +14,19 @@
 1996.03.22 labels.h created
 1996.04.03 fixed Borland C problem
 1997.01.26 speed tweak
+1997.02.24 converted to cvrotgfx
+1997.05.08 fettled
 */
 
 #include <string.h>
 #include "error.h"
 #include "caverot.h"
+#include "cvrotgfx.h"
 #include "labels.h"
 
 #if (OS==MSDOS)
-# define really_plot(S,X,Y) outtextxy((X)+(unsigned)xcMac/2,(Y)+(unsigned)ycMac/2,(S))
+# define really_plot(S,X,Y) outtextxy( (X) + (unsigned)xcMac / 2u,\
+                                       (Y) + (unsigned)ycMac / 2u, (S) )
 #endif
 
 static char **map;
@@ -31,21 +35,21 @@ static int width, height;
 extern int xcMac, ycMac;
 
 void init_map( void ) {
-  int i;
-  width=(unsigned)xcMac/4;
-  height=(unsigned)ycMac/4;
-  map=osmalloc(height*ossizeof(void*));
-  if (!map)
-    exit(1);
-  for( i=0 ; i<height ; i++ )
-    if ((map[i]=osmalloc(width*ossizeof(char)))==NULL)
+   int i;
+   width = (unsigned)xcMac / 4u;
+   height = (unsigned)ycMac / 4u;
+   map = osmalloc(height * ossizeof(void*));
+   if (!map)
       exit(1);
+   for( i = 0; i < height; i++ )
+      if ((map[i] = osmalloc(width * ossizeof(char))) == NULL)
+         exit(1);
 }
 
 void clear_map( void ) {
-  int i;
-  for( i=0 ; i<height ; i++ )
-    memset(map[i],0,width);
+   int i;
+   for( i = 0; i < height; i++ )
+      memset( map[i], 0, width );
 }
 
 #if (OS==RISCOS)
@@ -61,25 +65,26 @@ void fancy_label( char *label, int x, int y ) {
    extern void really_plot( char * /*label*/, int /*x*/, int /*y*/ );
 #endif
    if (!fAllNames) {
-      unsigned int X,Y; /* use unsigned so we can test for <0 for free */
-      int len,l,m;
-      X=(unsigned)(x+(unsigned)xcMac/2u)/4u;
-      if (X>=width)
+      unsigned int X, Y; /* use unsigned so we can test for <0 for free */
+      int len, l, m;
+      X = (unsigned)(x + (unsigned)xcMac / 2u) / 4u;
+      if (X >= width)
          return;
-      Y=(unsigned)(y+(unsigned)ycMac/2u)/4u;
-      if (Y>=height)
+      Y = (unsigned)(y + (unsigned)ycMac / 2u) / 4u;
+      if (Y >= height)
          return;
-      len=(strlen(label)*2)+1;
-      if (X+len>=width) len=width-X; /* or just 'return;' to right clip */
-      for( l=len-1 ; l>=0 ; l-- )
+      len = (strlen(label) * 2) + 1;
+      if ( X + len >= width) len = width - X; /* or 'return' to right clip */
+      for ( l = len - 1; l >= 0; l-- )
          if (map[Y][X+l])
             return;
-      l=Y-2;
-      if (l<0) l=0;
-      m=Y+2;
-      if (m>=height) m=height-1;
-      for( ; l<=m ; l++ )
-         memset( map[l]+X, 1, len);
+      l = Y - 2;
+      if (l < 0) l = 0;
+      m = Y + 2;
+      if (m >= height)
+         m = height - 1;
+      for( ; l <= m; l++ )
+         memset( map[l] + X, 1, len );
    }
    really_plot( label, x, y );
 }
