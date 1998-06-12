233fbf792cbc417587c1aec7cdb42dc4bc1b33c8
Olly Betts <olly@survex.com>
1998-06-12 16:19:32 +0000

    invert_var now external and returns 0 if matrix is singular added subvv
    git-svn-id: file:///home/survex-svn/survex/trunk@46 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/netbits.h
+++ src/netbits.h
@@ -1,12 +1,14 @@
 /* > netbits.h
  * Header file for miscellaneous primitive network routines for Survex
- * Copyright (C) 1994,1997 Olly Betts
+ * Copyright (C) 1994,1997,1998 Olly Betts
  */
 
 /*
 1994.04.16 created
 1994.05.07 added copy_link and addto_link
 1997.08.22 added covariances
+1998.04.28 invert_var now external and returns 0 if matrix is singular
+1998.04.29 added subvv
 */
 
 extern node *StnFromPfx( prefix *name );
@@ -18,13 +20,19 @@ extern char freeleg( node **stnptr );
 
 extern void addleg( node *fr, node *to,
                     real dx, real dy, real dz,
-                    real vx, real vy, real vz,
-                    real cyz, real czx, real cxy );
+                    real vx, real vy, real vz
+#ifndef NO_COVARIANCES
+		    , real cyz, real czx, real cxy
+#endif
+		   );
 
 extern void addfakeleg( node *fr, node *to,
 		        real dx, real dy, real dz,
-		        real vx, real vy, real vz,
-		        real cyz, real czx, real cxy );
+		        real vx, real vy, real vz
+#ifndef NO_COVARIANCES
+		        , real cyz, real czx, real cxy
+#endif
+		       );
 
 #define FOR_EACH_STN(S) for( (S)=stnlist ; (S)!=NULL ; (S)=(S)->next )
 #define print_prefix(N) fprint_prefix( stdout, (N) )
@@ -32,8 +40,11 @@ extern void addfakeleg( node *fr, node *to,
 extern char *sprint_prefix( prefix *ptr );
 extern void fprint_prefix( FILE *fh, prefix *ptr );
 
+/* extern for debugging */
+/* extern void print_var( var *a ); */
+
 /* r = ab ; r,a,b are variance matrices */
-void mulvv( var *r, var *a, var *b );
+void mulvv( var *r, const var *a, const var *b );
 
 /* r = ab ; r,b delta vectors; a variance matrix */
 void mulvd( d *r, var *a, d *b );
@@ -47,11 +58,19 @@ void subdd( d *r, d *a, d *b );
 /* r = a + b ; r,a,b variance matrices */
 void addvv( var *r, var *a, var *b );
 
+/* r = a - b ; r,a,b variance matrices */
+void subvv( var *r, var *a, var *b );
+
 /* r = a(b^-1) ; r,a delta vectors; b variance matrix */
 void divdv( d *r, d *a, var *b );
 
 /* r = a(b^-1) ; r,a,b variance matrices */
 void divvv( var *r, var *a, var *b );
+
+#ifndef NO_COVARIANCES
+/* inv = v^-1 ; inv,v variance matrices */
+int invert_var( var *inv, const var *v );
+#endif
 
 /* Is v zero? */
 bool fZero( var *v );
