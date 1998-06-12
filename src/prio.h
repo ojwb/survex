c4eac965cf3ed3ce13d13c7c2117fa03760c6de8
Olly Betts <olly@survex.com>
1998-06-12 16:19:39 +0000

    prio_print added
    git-svn-id: file:///home/survex-svn/survex/trunk@53 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/prio.h
+++ src/prio.h
@@ -7,6 +7,7 @@
 1993.07.12 split from printer drivers
 1993.08.15 standardised header
 1996.02.10 added pstr typedef (Pascal-style counted string) and prio_putpstr
+1998.03.21 added prio_print
 */
 
 #include <stdio.h>
@@ -21,5 +22,6 @@ typedef struct {
 extern void prio_open( sz fnmPrn );
 extern void prio_close( void );
 extern void prio_putc( int ch );
-extern void prio_printf( const char * format, ... );
+extern void prio_printf( const char *format, ... );
+extern void prio_print( const char *str );
 extern void prio_putpstr( pstr *p );
