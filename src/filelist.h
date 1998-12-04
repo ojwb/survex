8291acf99efdfad30fdbff2316a14f23322540cc
Olly Betts <olly@survex.com>
1998-12-04 22:26:23 +0000

    trimmed out obselete stuff
    git-svn-id: file:///home/survex-svn/survex/trunk@99 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/filelist.h
+++ src/filelist.h
@@ -25,11 +25,9 @@
 1996.02.27 PRINT_INI added
 */
 
-/* *_CFG now unused */
-
 #include "whichos.h"
 
-#if   (OS==RISCOS)
+#if (OS==RISCOS)
 
 # define ERRSTAT_FILE "ErrStats"
 # define STATS_FILE   "Stats"
@@ -37,10 +35,6 @@
 # define POSLIST_FILE "PosList"
 # define MESSAGE_FILE "Messages"
 
-# define PCL_CFG  "pclcfg"
-# define DM_CFG   "dmcfg"
-# define PS_CFG   "pscfg"
-# define HPGL_CFG "hpglcfg"
 # define PRINT_INI "print/ini"
 
 # define EXT_SVX_DATA "/svx" /* allows files to be read from DOS discs */
@@ -49,7 +43,7 @@
 # define EXT_SVX_STAT "/inf"
 # define EXT_SVX_POS  "/pos"
 
-#elif (OS==MSDOS || OS==TOS || OS==WIN32)
+#elif (OS==MSDOS || OS==TOS || OS==WIN32 || OS==UNIX || OS==AMIGA)
 
 # define ERRSTAT_FILE "errstats.txt"
 # define STATS_FILE   "stats.txt"
@@ -57,50 +51,6 @@
 # define POSLIST_FILE "poslist.txt"
 # define MESSAGE_FILE "messages.txt"
 
-# define PCL_CFG  "pcl.cfg"
-# define DM_CFG   "dm.cfg"
-# define PS_CFG   "ps.cfg"
-# define HPGL_CFG "hpgl.cfg"
-# define PRINT_INI "print.ini"
-
-# define EXT_SVX_DATA ".svx"
-# define EXT_SVX_3D   ".3d"
-# define EXT_SVX_ERRS ".err"
-# define EXT_SVX_STAT ".inf"
-# define EXT_SVX_POS  ".pos"
-
-#elif (OS==UNIX)
-
-# define ERRSTAT_FILE "errstats.txt"
-# define STATS_FILE   "stats.txt"
-# define IMAGE_FILE   "image.3d"
-# define POSLIST_FILE "poslist.txt"
-# define MESSAGE_FILE "messages.txt"
-
-# define PCL_CFG  "pcl.cfg"
-# define DM_CFG   "dm.cfg"
-# define PS_CFG   "ps.cfg"
-# define HPGL_CFG "hpgl.cfg"
-# define PRINT_INI "print.ini"
-
-# define EXT_SVX_DATA ".svx"
-# define EXT_SVX_3D   ".3d"
-# define EXT_SVX_ERRS ".err"
-# define EXT_SVX_STAT ".inf"
-# define EXT_SVX_POS  ".pos"
-
-#elif (OS==AMIGA)
-
-# define ERRSTAT_FILE "errstats.txt"
-# define STATS_FILE   "stats.txt"
-# define IMAGE_FILE   "image.3d"
-# define POSLIST_FILE "poslist.txt"
-# define MESSAGE_FILE "messages.txt"
-
-# define PCL_CFG  "pcl.cfg"
-# define DM_CFG   "dm.cfg"
-# define PS_CFG   "ps.cfg"
-# define HPGL_CFG "hpgl.cfg"
 # define PRINT_INI "print.ini"
 
 # define EXT_SVX_DATA ".svx"
