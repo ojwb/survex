ee7cce1b9e40ae1439bb1c6e78da393ea499823c
Olly Betts <olly@survex.com>
1999-04-30 16:33:48 +0000

    fixed riscos warnings fixed riscos M and R button swap cleaned up somewhat
    git-svn-id: file:///home/survex-svn/survex/trunk@167 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/cvrotgfx.h
+++ src/cvrotgfx.h
@@ -5,6 +5,8 @@
 #ifndef SVX_CVROTGFX_H
 #define SVX_CVROTGFX_H 1
 
+/* FIXME: sort out _cvrotgfx_textcol/_cvrotgfx_drawcol ugliness */
+
 #include "whichos.h"
 
 extern int mouse_buttons;
@@ -14,17 +16,6 @@ extern int colText, colHelp;
 #ifdef __DJGPP__
 # define ALLEGRO 1
 #endif
-
-#if (OS==UNIX)
-
-/* UNIX + Allegro */
-# include "allegro.h"
-# define ALLEGRO 1
-extern BITMAP *BitMap, *BitMapDraw;
-
-# define CVROTGFX_LBUT 1 /* mask for left mouse button */
-# define CVROTGFX_RBUT 2 /* mask for right mouse button (if present) */
-# define CVROTGFX_MBUT 4 /* mask for middle mouse button (if present) */
 
 extern int cvrotgfx_parse_cmdline(int *pargc, char **argv);
 extern int cvrotgfx_init(void);
@@ -39,6 +30,17 @@ extern void (cvrotgfx_beep)(void); /* make a beep */
 extern void cvrotgfx_moveto(int X, int Y);
 extern void cvrotgfx_lineto(int X, int Y);
 
+#if (OS==UNIX)
+
+/* UNIX + Allegro */
+# include "allegro.h"
+# define ALLEGRO 1
+extern BITMAP *BitMap, *BitMapDraw;
+
+# define CVROTGFX_LBUT 1 /* mask for left mouse button */
+# define CVROTGFX_RBUT 2 /* mask for right mouse button (if present) */
+# define CVROTGFX_MBUT 4 /* mask for middle mouse button (if present) */
+
 # define cvrotgfx_beep() NOP /* !HACK! */
 
 # ifdef NO_MOUSE_SUPPORT
@@ -46,7 +48,6 @@ extern void cvrotgfx_lineto(int X, int Y);
 #  define cvrotgfx_read_mouse(PDX, PDY, PBUT) NOP
 # endif
 
-/*# define cleardevice() GrClearContext(0)*/
 # ifdef NO_TEXT
 #  define outtextxy(X, Y, SZ) NOP
 # else
@@ -92,19 +93,6 @@ extern GrContext *BitMap;
 
 # endif
 
-extern int cvrotgfx_parse_cmdline(int *pargc, char **argv);
-extern int cvrotgfx_init(void);
-extern int cvrotgfx_pre_main_draw(void);
-extern int cvrotgfx_post_main_draw(void);
-extern int cvrotgfx_pre_supp_draw(void);
-extern int cvrotgfx_post_supp_draw(void);
-extern int cvrotgfx_final(void);
-extern int cvrotgfx_get_key(void);
-extern void cvrotgfx_read_mouse(int *pdx, int *pdy, int *pbut);
-extern void (cvrotgfx_beep)(void); /* make a beep */
-extern void cvrotgfx_moveto(int X, int Y);
-extern void cvrotgfx_lineto(int X, int Y);
-
 # ifdef NO_MOUSE_SUPPORT
 /* emulate a dead mouse */
 #  define cvrotgfx_read_mouse(PDX, PDY, PBUT) NOP
@@ -123,7 +111,6 @@ extern void cvrotgfx_lineto(int X, int Y);
 #  define CVROTGFX_RBUT 2 /* mask for right mouse button (if present) */
 #  define CVROTGFX_MBUT 4 /* mask for middle mouse button (if present) */
 # endif
-
 
 # define cvrotgfx_beep() sound(256) /* 256 is frequency */
 
@@ -152,20 +139,17 @@ extern void cvrotgfx_lineto(int X, int Y);
 # endif
 
 # ifdef MSC
-/*# define cleardevice() _clearscreen(_GCLEARSCREEN)*/
 #  define outtextxy(X, Y, S) BLK(cvrotgfx_moveto(X, Y); _outgtext(S);)
 #  define set_tcolour(X) _setcolor(X)
 #  define set_gcolour(X) _setcolor(X)
 #  define text_xy(X, Y, S) outtextxy((X) * 12, (Y) * 12, S)
 # elif defined(JLIB)
-/*#  define cleardevice() buff_clear(BitMap)*/
 #  define outtextxy(X, Y, S) buff_draw_string(BitMap, (S), (X), (Y), 15); /*15 is colour !HACK!*/
 #  define set_tcolour(X) /* !!HACK!! */
 #  define set_gcolour(X) BLK(extern int _cvrotgfx_drawcol; _cvrotgfx_drawcol = (X);)
 #  define text_xy(X, Y, S) outtextxy(12 + (X) * 12, 12 + (Y) * 12, (S))
 #  define far
 # elif defined(ALLEGRO)
-/*# define cleardevice() GrClearContext(0)*/
 #  ifdef NO_TEXT
 #   define outtextxy(X, Y, S) NOP
 #  else
@@ -177,7 +161,6 @@ extern void cvrotgfx_lineto(int X, int Y);
 #  define set_gcolour(X) BLK(extern int _cvrotgfx_drawcol; _cvrotgfx_drawcol = (X);)
 #  define text_xy(X, Y, S) outtextxy(12 + (X) * 12, 12 + (Y) * 12, S)
 # elif defined(__DJGPP__)
-/*#  define cleardevice() GrClearContext(0)*/
 #  define outtextxy(X, Y, S) GrTextXY((X), (Y), (S), 15, 0) /* !HACK! 15 and 0 are colours */
 #  define set_tcolour(X) /* !!HACK!! */
 #  define set_gcolour(X) /* !!HACK!! */
@@ -190,8 +173,6 @@ extern void cvrotgfx_lineto(int X, int Y);
 # undef  Y_UP /* PC has increasing Y down screen */
 
 #elif (OS==RISCOS)
-/* # include "bbc.h" */
-/* # include "kernel.h" */
 
 # undef bool /* bloody namespace poluting libraries */
 # include "oslib/os.h"
@@ -204,15 +185,13 @@ extern void text_xy(int x, int y, const char *str);
 
 # define set_gcolour(X) (ol_setcol((X)))
 # define set_tcolour(X) (xos_set_gcol(), xos_writec(0), xos_writec((X)))
-/* # define cleardevice() (xos_cls()) */
-/* # define shift_pressed() (bbc_inkey(-1)) */
 # define shift_pressed() (osbyte_read(osbyte_VAR_KEYBOARD_STATE) & 0x08)
 
 # define cvrotgfx_beep() xos_bell()
 
 # define CVROTGFX_LBUT 4
-# define CVROTGFX_MBUT 1
-# define CVROTGFX_RBUT 2
+# define CVROTGFX_MBUT 2
+# define CVROTGFX_RBUT 1
 
 #else
 # error Operating System not known
