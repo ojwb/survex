a0b20dcac4df8adcf757075db9c87c8b7df3ea18
Olly Betts <olly@survex.com>
1999-09-04 11:40:04 +0000

    more work to get everything into CVS
    git-svn-id: file:///home/survex-svn/survex/trunk@270 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

---
+++ acconfig.h
@@ -0,0 +1,15 @@
+/* copyright message */
+#undef COPYRIGHT_MSG
+
+/* package name */
+#undef PACKAGE
+
+/* package version */
+#undef VERSION 
+
+/* location of platform independent data files */
+#undef SURVEXHOME
+
+/* strcasecmp define for systems which call it something else */
+#undef strcasecmp
+
