3573dede262ef1519bb70866817f53399b956d0d
Olly Betts <olly@survex.com>
2000-01-21 15:04:17 +0000

    Added fix for enum BOOL problem when included in C++ code
    git-svn-id: file:///home/survex-svn/survex/trunk@330 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/ostypes.h
+++ src/ostypes.h
@@ -6,8 +6,13 @@
 #ifndef OSTYPES_H /* only include once */
 #define OSTYPES_H
 
+#ifdef __cplusplus
 typedef enum { fFalse=0, fTrue=1 } BOOL;
 # define bool BOOL
+#else
+#define fFalse false
+#define fTrue true
+#endif
 typedef unsigned char UCHAR;
 typedef unsigned long ULONG;
 typedef unsigned int UINT;
