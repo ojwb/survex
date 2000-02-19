89231c4e39bff0d237a4178a0fc4eaa36e13d1f5
Olly Betts <olly@survex.com>
2000-02-19 23:30:13 +0000

    Updated GPL info (now has correct address for FSF)
    git-svn-id: file:///home/survex-svn/survex/trunk@378 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/aven.cc
+++ src/aven.cc
@@ -4,23 +4,22 @@
 //  A cave visualisation application for use in conjunction with Survex and
 //  the GTK+ toolkit.
 //
-//  (c) Copyright 1999-2000, Mark R. Shinwell.
-//  Portions from Survex (c) Copyright Olly Betts and others.
+//  Copyright (C) 1999-2000, Mark R. Shinwell.
+//  Portions from Survex Copyright (C) Olly Betts and others.
 //
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public
-//  License as published by the Free Software Foundation; either
-//  version 2 of the License, or (at your option) any later version.
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  General Public License for more details.
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public
-//  License along with this program; if not, write to the Free
-//  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 // Vastly improved speed is obtained with this #defined...
 #define NASTY_SPEED_HACK
