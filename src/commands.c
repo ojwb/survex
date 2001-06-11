289b7a8bc1b721cc2673d11b346bf87fb74b44e4
Olly Betts <olly@survex.com>
2001-06-11 06:58:05 +0000

    cavern: after a bad *data command, ignore survey data until the next valid *data command.
    git-svn-id: file:///home/survex-svn/survex/trunk@861 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/commands.c
+++ src/commands.c
@@ -962,6 +962,10 @@ cmd_data(void)
    unsigned long m, mUsed = 0;
    char *style_name;
 
+   /* after a bad *data command ignore survey data until the next              
+    * *data command to avoid an avalanche of errors */                         
+   pcs->Style = style_ignore;
+
    kMac = 6; /* minimum for NORMAL style */
    new_order = osmalloc(kMac * sizeof(reading));
 
