c87107ab76caa5daaefa917cc2652767537d8c78
Olly Betts <olly@survex.com>
2001-09-21 23:22:01 +0000

    Aligned preprocessor "#"s to left column...
    git-svn-id: file:///home/survex-svn/survex/trunk@1400 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/wx.h
+++ src/wx.h
@@ -1,11 +1,11 @@
 #include "wx/wxprec.h"
 
 #ifdef __BORLANDC__
-    #pragma hdrstop
+# pragma hdrstop
 #endif
 
 #ifndef WX_PRECOMP
-    #include "wx/wx.h"
+# include "wx/wx.h"
 #endif
 
 #include "wx/splitter.h"
