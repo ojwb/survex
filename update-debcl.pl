15f8095be69add5423ccf838edfb55f8135ce70b
Olly Betts <olly@survex.com>
2001-10-05 00:03:51 +0000

    Script to update debian changelog.
    git-svn-id: file:///home/survex-svn/survex/trunk@1486 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

---
+++ update-debcl.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+require 5.003;
+use strict;
+
+my $package = '';
+my $version = '';
+open C, "<configure.in" or die $!;
+while (<C>) {
+   if (/^AM_INIT_AUTOMAKE\(\s*([^,]+?)\s*,\s*([\d.]+)\)/) {
+      $package = $1;
+      $version = $2;
+      last;
+   }
+}
+close C;
+
+open M, "<Makefile.am" or die $!;
+while (<M>) {
+   if (/^RELEASE\s*=\s*([0-9][0-9]*)/) {
+      $version .= ".$1";
+      last;
+   }
+}
+close M;
+
+open CL, "<debian/changelog" or die $!;
+
+my $date = strftime "+%a, %d %b %Y %X %z", localtime;
+
+my $line = <CL>;
+unless ($line =~ /^\Q$package ($version)/) {
+   open O, ">debian/changelog~" or die $!;
+   print O <<END;
+$package ($version) unstable; urgency=low
+
+  * New release
+
+ -- Olly Betts <olly\@survex.com>  $date
+
+END
+   close O;
+   print O $line;
+   while (<CL>) {
+      print O $_;
+   }
+   close CL;
+   close O;
+   rename "debian/changelog~", "debian/changelog" or die $!;
+}
