5a9b8cc51ec62238ef7c8d92700936c4b22d1ad7
Olly Betts <olly@survex.com>
2002-03-21 01:17:29 +0000

    1.0.3
    git-svn-id: file:///home/survex-svn/survex/trunk@1777 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/mainfrm.h
+++ src/mainfrm.h
@@ -4,6 +4,7 @@
 //  Main frame handling for Aven.
 //
 //  Copyright (C) 2000-2001, Mark R. Shinwell.
+//  Copyright (C) 2001-2002 Olly Betts
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
