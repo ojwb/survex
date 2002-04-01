6deca307847071b04988690c3619d319f9940026
Olly Betts <olly@survex.com>
2002-04-01 12:27:00 +0000

    Run checkshortcut.pl automatically for "make dist"
    git-svn-id: file:///home/survex-svn/survex/trunk@1827 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- lib/checkshortcut.pl
+++ lib/checkshortcut.pl
@@ -1,15 +1,22 @@
 #!/usr/bin/perl -w
 use strict;
 my %menu = ();
-open I, "<../src/mainfrm.cc" or die $!;
+my $srcdir = $ENV{'srcdir'};
+$srcdir .= '/' if defined $srcdir;
+
+open I, "<$srcdir../src/mainfrm.cc" or die $!;
 while (<I>) {
     /(\w+)->Append\(.*\(\/\*.*?\*\/(\d+)/ && push @{$menu{$1}}, $2;
 }
 close I;
 
+my $exitcode = 0;
 for my $lang (@ARGV) {
-    print "Lang $lang:\n";
-    open L, "<$lang.msg" or die $!;
+    $lang =~ s/\.msg$//; # allow en or en.msg
+    my $hdr = "Lang $lang:\n";
+    # .msg files could be in srcdir or build directory when building outside
+    # the source tree
+    open L, "<$srcdir$lang.msg" or open L, "<$lang.msg" or die $!;
     my $buf;
     read L, $buf, 20;
     read L, $buf, 999999;
@@ -22,6 +29,10 @@ for my $lang (@ARGV) {
             my ($acc) = ($msg[$_] =~ /\@(.)/);
 	    $acc = lc $acc;
 	    if (exists $sc{$acc}) {
+	        if (defined $hdr) {
+		    print $hdr;
+		    $hdr = undef;
+		}
 		print "Menu $menu : '$msg[$sc{$acc}]' and '$msg[$_]' clash\n";
 		$bad = 1;
 	    } else {
@@ -30,8 +41,9 @@ for my $lang (@ARGV) {
 	}
 	if ($bad) {
 	    print "Unused letters: ", grep {!exists $sc{$_}} ('a' .. 'z'), "\n";
+	    $exitcode = 1;
 	}
     }
-    print "\n";
+    print "\n" unless defined $hdr;
 }
-
+exit $exitcode;
