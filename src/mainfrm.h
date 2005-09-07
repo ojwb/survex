abfd57a5746d34cb719ffd0c427cebf62842f073
Olly Betts <olly@survex.com>
2005-09-07 16:29:36 +0000

    wxPageSetupData -> wxPageSetupDialogData
    git-svn-id: file:///home/survex-svn/survex/trunk@2944 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/mainfrm.h
+++ src/mainfrm.h
@@ -4,7 +4,7 @@
 //  Main frame handling for Aven.
 //
 //  Copyright (C) 2000-2001, Mark R. Shinwell.
-//  Copyright (C) 2001-2003,2004 Olly Betts
+//  Copyright (C) 2001-2003,2004,2005 Olly Betts
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -191,7 +191,7 @@ private:
 	Double x, y, z;
     } m_Offsets;
 
-    wxPageSetupData m_pageSetupData;
+    wxPageSetupDialogData m_pageSetupData;
     wxPrintData m_printData;
 
     wxString icon_path;
@@ -423,7 +423,7 @@ public:
     void ShowInfo(const LabelInfo *label);
     void DisplayTreeInfo(const wxTreeItemData* data);
     void TreeItemSelected(wxTreeItemData* data);
-    wxPageSetupData * GetPageSetupData() { return &m_pageSetupData; }
+    wxPageSetupDialogData * GetPageSetupData() { return &m_pageSetupData; }
 
 private:
     DECLARE_EVENT_TABLE()
