8c05f1339b4fe277d91192fd0d2a2b673ed57edf
Olly Betts <olly@survex.com>
2005-10-08 21:00:55 +0000

    Fix "" which were appearing in debian/copyright, eliminating COPYRIGHT_MSG_ASCII and AVEN_COPYRIGHT_MSG_ASCII in the process.
    git-svn-id: file:///home/survex-svn/survex/branches/survex-1_1@3048 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/aboutdlg.cc
+++ src/aboutdlg.cc
@@ -62,9 +62,17 @@ AboutDlg::AboutDlg(wxWindow* parent, const wxString & icon_path) :
     }
     title->Add(new wxStaticText(this, 502, id), 0, wxALL, 2);
 
-    wxStaticText* copyright = new wxStaticText(this, 503,
-	    wxString::Format(AVEN_COPYRIGHT_MSG"\n"COPYRIGHT_MSG,
-			     msg(/*&copy;*/0), msg(/*&copy;*/0)));
+    wxString copyright_msg = COPYRIGHT_MSG"\n"AVEN_COPYRIGHT_MSG;
+    const char * csign = msg(/*&copy;*/0);
+    if (strcmp(csign, "(C)") != 0) {
+	size_t csign_len = strlen(csign);
+	size_t i = 0;
+	while ((i = copyright_msg.find("(C)", i)) != wxString::npos) {
+	    copyright_msg.replace(i, 3, csign, csign_len);
+	    i += csign_len;
+	}
+    }
+    wxStaticText* copyright = new wxStaticText(this, 503, copyright_msg);
 
     wxString licence_str;
     wxString l(msg(/*This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public Licence as published by the Free Software Foundation; either version 2 of the Licence, or (at your option) any later version.*/219));
