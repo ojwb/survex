78bae73c0e95e05f081344856da0a9a487d2654e
Olly Betts <olly@survex.com>
2010-04-17 12:00:43 +0000

    Update (C) date.
    git-svn-id: file:///home/survex-svn/survex/branches/survex-1_1@3353 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/aboutdlg.cc
+++ src/aboutdlg.cc
@@ -4,7 +4,7 @@
 //  About box handling for Aven.
 //
 //  Copyright (C) 2001-2003 Mark R. Shinwell.
-//  Copyright (C) 2001,2002,2003,2004,2005,2006 Olly Betts
+//  Copyright (C) 2001,2002,2003,2004,2005,2006,2010 Olly Betts
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
