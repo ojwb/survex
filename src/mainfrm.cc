de402d4dbcc42f36192a0bef199364b5c842ff3c
Olly Betts <olly@survex.com>
2011-09-16 03:18:49 +0000

    src/mainfrm.cc: If a label isn't valid UTF-8, treat it as CP1252 (the Microsoft superset of ISO8859-1).
    git-svn-id: file:///home/survex-svn/survex/trunk@3639 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/mainfrm.cc
+++ src/mainfrm.cc
@@ -1123,7 +1123,15 @@ bool MainFrm::LoadData(const wxString& file, wxString prefix)
 		if (flags & img_SFLAG_ENTRANCE) {
 		    flags ^= (img_SFLAG_ENTRANCE | LFLAG_ENTRANCE);
 		}
-		LabelInfo* label = new LabelInfo(pt, wxString(survey->label, wxConvUTF8), flags);
+		wxString s(survey->label, wxConvUTF8);
+		if (s.empty()) {
+		    // If label isn't valid UTF-8 then this conversion will
+		    // give an empty string.  In this case, assume that the label
+		    // is CP1252 (the Microsoft superset of ISO8859-1).
+		    static wxCSConv ConvCP1252(wxFONTENCODING_CP1252);
+		    s = wxString(survey->label, ConvCP1252);
+		}
+		LabelInfo* label = new LabelInfo(pt, s, flags);
 		if (label->IsEntrance()) {
 		    m_NumEntrances++;
 		}
