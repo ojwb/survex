c930b7f898dd84b7563c7a9c0182827322cade4d
Olly Betts <olly@survex.com>
2011-10-06 04:13:54 +0000

    src/gettexttomsg.pl: Update for the message.txt -> codes.po change.
    git-svn-id: file:///home/survex-svn/survex/trunk@3847 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/gettexttomsg.pl
+++ src/gettexttomsg.pl
@@ -2,7 +2,7 @@
 
 #  gettexttomsg.pl
 #
-#  Copyright (C) 2001,2002,2005 Olly Betts
+#  Copyright (C) 2001,2002,2005,2011 Olly Betts
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -19,67 +19,28 @@
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 require 5.008;
-use bytes;
 use strict;
-
-use integer;
-
-# Magic identifier (12 bytes)
-my $magic = "Svx\nMsg\r\n\xfe\xff\0";
-# Designed to be corrupted by ASCII ftp, top bit stripping (or
-# being used for parity).  Contains a zero byte so more likely
-# to be flagged as data (e.g. by perl's "-B" test).
-
-my $major = 0;
-my $minor = 8;
-
-# File format (multi-byte numbers in network order (bigendian)):
-# 12 bytes: Magic identifier
-# 1 byte:   File format major version number (0)
-# 1 byte:   File format minor version number (8)
-# 2 bytes:  Number of messages (N)
-# 4 bytes:  Offset from XXXX to end of file
-# XXXX:
-# N*:
-# <message> NUL
-
-my %ent = ();
-
-open ENT, "../lib/named-entities.txt" or die $!;
-while (<ENT>) {
-   my ($e, $v) = /^(\w+),(\d+)/;
-   $ent{$e} = $v;
-}
-close ENT;
 
 my %revmsgs = ();
 
-open MSG, "../lib/messages.txt" or die $!;
+my $msgid;
+open MSG, "../lib/codes.po" or die $!;
 while (<MSG>) {
-   next if /^\s*#/; # skip comments
+    next if /^\s*#/; # skip comments
    
-   # en:  0 0.81 the message
-   # en-us: 0 0.81 " the message "
-   my ($langs, $msgno, $dummy, $msg) = /^([-\w,]+):\s*(\d+)\s+("?)(.*)\3/;
-
-   unless (defined $langs) {
-      chomp;
-      print STDERR "Warning: Bad line: \"$_\"\n";
-      next;
-   }
-
-   $langs =~ tr/-/_/;
-
-   if ($msg =~ /[\0-\x1f\x7f-\xff]/) {
-      print STDERR "Warning: literal character in message $msgno\n";
-   }
-
-   my $utf8 = string_to_utf8($msg);
-   for (split /,/, $langs) {
-      if (lc $_ eq 'en') {
-	  $revmsgs{$utf8} = $msgno;
-      }
-   }
+    if (/^\s*msgid\s*"(.*)"/) {
+	$msgid = $1;
+    } elsif (/^\s*msgstr\s*"(.*)"/) {
+	my $msgstr = $1;
+	if (! defined $msgid) {
+	    die "No msgid for msgstr '$msgstr'\n";
+	}
+	if ($msgstr !~ /^\d*$/) {
+	    die "Non-numeric msgstr '$msgstr'\n";
+	}
+	$revmsgs{$msgid} = $msgstr;
+	$msgid = undef;
+    }
 }
 
 my $die = 0;
@@ -104,8 +65,6 @@ if ($die) {
 sub replacement {
     my $msg = shift;
     $msg =~ s/\\\n//g;
-    $msg =~ s/\\n/\n/g;
-    $msg =~ s/\\t/\t/g;
     my $msgno = "";
     if (exists $revmsgs{$msg}) {
 	$msgno = $revmsgs{$msg};
@@ -116,41 +75,5 @@ sub replacement {
 	}
 	print STDERR "'$msg'\n";
     }
-    $msg =~ s/\n/&#10;/g;
-    $msg =~ s/\t/&#9;/g;
     return "msg(/*$msg*/$msgno)";
-}
-
-sub string_to_utf8 {
-   my $s = shift;
-   $s =~ s/([\x80-\xff])/char_to_utf8(ord($1))/eg;
-   $s =~ s/\&(#\d+|#x[a-f0-9]+|[a-z0-9]+);?/decode_entity($1)/eig;
-   return $s;
-}
-
-sub decode_entity {
-   my $ent = shift;
-   return char_to_utf8($1) if $ent =~ /^#(\d+)$/;
-   return char_to_utf8(hex($1)) if $ent =~ /^#x([a-f0-9]+)$/;
-   return char_to_utf8($ent{$ent}) if exists $ent{$ent};
-   $ent = "\&$ent;";
-   print STDERR "Warning: entity \"$ent\" unknown\n";
-   return $ent;
-}
-
-sub char_to_utf8 {
-   my $unicode = shift;
-   # ASCII is easy, and the most common case
-   return chr($unicode) if $unicode < 0x80;
-
-   my $result = '';
-   my $n = 0x20;
-   while (1) {
-      $result = chr(0x80 | ($unicode & 0x3f)) . $result;
-      $unicode >>= 6;
-      last if $unicode < $n;
-      $n >>= 1;
-   }
-   $result = chr((0x100 - $n*2) | $unicode) . $result;
-   return $result;
 }
