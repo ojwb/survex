6e0c5879199c3adadc1a815623573a42f518aec9
Olly Betts <olly@survex.com>
2012-01-01 02:53:20 +0000

    src/gfxcore.cc,src/gfxcore.h: Update copyright years.
    git-svn-id: file:///home/survex-svn/survex/trunk@3923 4b37db11-9a0c-4f06-9ece-9ab7cdaee568

--- src/gfxcore.cc
+++ src/gfxcore.cc
@@ -4,7 +4,7 @@
 //  Core drawing code for Aven.
 //
 //  Copyright (C) 2000-2003,2005,2006 Mark R. Shinwell
-//  Copyright (C) 2001-2003,2004,2005,2006,2007,2010,2011 Olly Betts
+//  Copyright (C) 2001-2003,2004,2005,2006,2007,2010,2011,2012 Olly Betts
 //  Copyright (C) 2005 Martin Green
 //
 //  This program is free software; you can redistribute it and/or modify
--- src/gfxcore.h
+++ src/gfxcore.h
@@ -4,7 +4,7 @@
 //  Core drawing code for Aven.
 //
 //  Copyright (C) 2000-2001,2002,2005 Mark R. Shinwell.
-//  Copyright (C) 2001-2004,2005,2006,2007,2010,2011 Olly Betts
+//  Copyright (C) 2001-2004,2005,2006,2007,2010,2011,2012 Olly Betts
 //  Copyright (C) 2005 Martin Green
 //
 //  This program is free software; you can redistribute it and/or modify
