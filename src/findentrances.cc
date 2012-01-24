1686083ecdc1ad0051c0828cb4e586488c05fa8e
Olly Betts <olly@survex.com>
2012-01-24 23:44:12 +1300

    src/findentrances.cc: Add GPL licence boilerplate (Olaf OKed this in <20120111115239.1b162450@ghost>).

--- src/findentrances.cc
+++ src/findentrances.cc
@@ -1,5 +1,25 @@
-/** Simple converter from survex .3d files to a list of entrances in GPX format
+/* findentrances.cc
+ * Simple converter from survex .3d files to a list of entrances in GPX format
  *
+ * Copyright (C) 2012 Olaf Kähler
+ * Copyright (C) 2012 Olly Betts
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
  * This program parses the survex file, creates a list of entrances and does
  * the coordinate transformations from almost arbitrary formats into WGS84
  * using the PROJ.4 library. The output is then written as a GPX file ready
