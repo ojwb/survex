84d5946703c2878bacbbe8fc578498ea171e7988
Olly Betts <olly@survex.com>
2014-07-03 00:03:56 +1200

    src/netskel.c: If there's an output coordinate system set, write it to the 3d file.

--- src/netskel.c
+++ src/netskel.c
@@ -439,7 +439,7 @@ replace_travs(void)
    if (!pimg) {
       char *fnm = add_ext(fnm_output_base, EXT_SVX_3D);
       filename_register_output(fnm);
-      pimg = img_open_write(fnm, survey_title, 0);
+      pimg = img_open_write_cs(fnm, survey_title, proj_str_out, 0);
       if (!pimg) fatalerror(img_error(), fnm);
       osfree(fnm);
    }
