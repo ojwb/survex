ce9cd1c12aec332e87283f32a13d58de9560b140
Olly Betts <olly@survex.com>
2015-01-21 22:41:41 +1300

    src/aboutdlg.cc: Fix "Close" button in about dialog under OS X.

--- src/aboutdlg.cc
+++ src/aboutdlg.cc
@@ -4,7 +4,7 @@
 //  About box handling for Aven.
 //
 //  Copyright (C) 2001-2003 Mark R. Shinwell.
-//  Copyright (C) 2001,2002,2003,2004,2005,2006,2010,2014 Olly Betts
+//  Copyright (C) 2001,2002,2003,2004,2005,2006,2010,2014,2015 Olly Betts
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -62,7 +62,7 @@ AboutDlg::OnCopy(wxCommandEvent &)
 void
 AboutDlg::OnClose(wxCommandEvent &)
 {
-    Destroy();
+    Close();
 }
 
 AboutDlg::AboutDlg(wxWindow* parent, const wxString & icon_path_) :
