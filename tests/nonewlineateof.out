ojwb/survex

commit 4b4ac38fca61b9bf0cbbefc17a00cfeaca3d2dfb

Simplify end of line handling
process_eol() no longer pushes back an EOL character, which simplifies
the code, and means process_bol() can be eliminated.

src/datain.c
@@ -130,7 +130,7 @@ compile_v_report(int severity, int en, va_list ap)
    error_list_parent_files();
    if (en < 0) {
       en = -en;
-      if (file.fh) col = ftell(file.fh) - file.lpos;
+      if (file.fh) col = ftell(file.fh) - file.lpos + 1;
    }
    v_report(severity, file.filename, file.line, col, en, ap);
 }
@@ -161,7 +161,7 @@ compile_error_reading(reading r, int en, ...)
    int col = 0;
    va_start(ap, en);
    error_list_parent_files();
-   if (LOC(r) > file.lpos) col = LOC(r) - file.lpos;
+   if (LOC(r) >= file.lpos) col = LOC(r) - file.lpos + 1;
    v_report(1, file.filename, file.line, col, en, ap);
    va_end(ap);
 }
@@ -173,7 +173,7 @@ compile_error_reading_skip(reading r, int en, ...)
    int col = 0;
    va_start(ap, en);
    error_list_parent_files();
-   if (LOC(r) > file.lpos) col = LOC(r) - file.lpos;
+   if (LOC(r) >= file.lpos) col = LOC(r) - file.lpos + 1;
    v_report(1, file.filename, file.line, col, en, ap);
    va_end(ap);
    skipline();
@@ -276,13 +276,6 @@ skipline(void)
 }
 
 static void
-process_bol(void)
-{
-   nextch();
-   skipblanks();
-}
-
-static void
 process_eol(void)
 {
    int eolchar;
@@ -302,7 +295,6 @@ process_eol(void)
    while (ch != EOF) {
       nextch();
       if (ch == eolchar || !isEol(ch)) {
-	 push_back(ch);
 	 break;
       }
       if (ch == '\n') eolchar = ch;
@@ -313,7 +305,7 @@ process_eol(void)
 static bool
 process_non_data_line(void)
 {
-   process_bol();
+   skipblanks();
 
    if (isData(ch)) return fFalse;
 
@@ -383,7 +375,6 @@ nextch_handling_eol(void)
    nextch();
    while (ch != EOF && isEol(ch)) {
       process_eol();
-      nextch();
    }
 }
 
@@ -429,6 +420,7 @@ data_file(const char *pth, const char *fnm)
       file.line = 1;
       file.lpos = 0;
       file.reported_where = fFalse;
+      nextch();
    }
 
    using_data_file(file.filename);
@@ -524,7 +516,6 @@ data_file(const char *pth, const char *fnm)
 	    CompassDATFlags, IgnoreAll
 	 };
 	 /* <Cave name> */
-	 process_bol();
 	 skipline();
 	 process_eol();
 	 /* SURVEY NAME: <Short name> */
@@ -597,19 +588,15 @@ data_file(const char *pth, const char *fnm)
 	 skipline();
 	 process_eol();
 	 /* BLANK LINE */
-	 process_bol();
 	 skipline();
 	 process_eol();
 	 /* heading line */
-	 process_bol();
 	 skipline();
 	 process_eol();
 	 /* BLANK LINE */
-	 process_bol();
 	 skipline();
 	 process_eol();
 	 while (!feof(file.fh)) {
-	    process_bol();
 	    if (ch == '\x0c') {
 	       nextch();
 	       process_eol();
@@ -1379,10 +1366,9 @@ data_cartesian(void)
 	 fMulti = fTrue;
 	 while (1) {
 	    process_eol();
-	    process_bol();
+	    skipblanks();
 	    if (isData(ch)) break;
 	    if (!isComm(ch)) {
-	       push_back(ch);
 	       return;
 	    }
 	 }
@@ -1398,7 +1384,7 @@ data_cartesian(void)
 	 }
 	 do {
 	    process_eol();
-	    process_bol();
+	    skipblanks();
 	 } while (isComm(ch));
 	 goto again;
        default: BUG("Unknown reading in ordering");
@@ -1783,10 +1769,9 @@ data_normal(void)
 	  fMulti = fTrue;
 	  while (1) {
 	      process_eol();
-	      process_bol();
+	      skipblanks();
 	      if (isData(ch)) break;
 	      if (!isComm(ch)) {
-		 push_back(ch);
 		 return;
 	      }
 	  }
@@ -1890,7 +1875,7 @@ data_normal(void)
 	  }
 	  do {
 	     process_eol();
-	     process_bol();
+	     skipblanks();
 	  } while (isComm(ch));
 	  goto again;
        default:
@@ -2026,10 +2011,9 @@ data_nosurvey(void)
 	 if (ordering[1] == End) {
 	    do {
 	       process_eol();
-	       process_bol();
+	       skipblanks();
 	    } while (isComm(ch));
 	    if (!isData(ch)) {
-	       push_back(ch);
 	       return;
 	    }
 	    goto again;
@@ -2037,10 +2021,9 @@ data_nosurvey(void)
 	 fMulti = fTrue;
 	 while (1) {
 	    process_eol();
-	    process_bol();
+	    skipblanks();
 	    if (isData(ch)) break;
 	    if (!isComm(ch)) {
-	       push_back(ch);
 	       return;
 	    }
 	 }
@@ -2056,7 +2039,7 @@ data_nosurvey(void)
 	 }
 	 do {
 	    process_eol();
-	    process_bol();
+	    skipblanks();
 	 } while (isComm(ch));
 	 goto again;
        default: BUG("Unknown reading in ordering");

commit 718e568403972c3a067cfcd979a296c9927d9440

Fix hang processing file without newline at end
Bug introduced in 4b4ac38fca61b9bf0cbbefc17a00cfeaca3d2dfb (and
release 1.2.28).

Reported by Mark Brown.

src/datain.c
@@ -129,8 +129,9 @@ show_line(int col, int width)
 
    /* Read the whole line and write it out. */
    PUTC(' ', STDERR);
-   while (!feof(file.fh)) {
+   while (1) {
       int c = GETC(file.fh);
+      /* Note: isEol() is true for EOF */
       if (isEol(c)) break;
       if (c == '\t') ++tabs;
       PUTC(c, STDERR);
@@ -611,7 +612,7 @@ data_file(const char *pth, const char *fnm)
 #endif
 
    if (fmt == FMT_DAT) {
-      while (!feof(file.fh) && !ferror(file.fh)) {
+      while (ch != EOF && !ferror(file.fh)) {
 	 static reading compass_order[] = {
 	    Fr, To, Tape, CompassDATComp, CompassDATClino,
 	    CompassDATLeft, CompassDATRight, CompassDATUp, CompassDATDown,
@@ -703,7 +704,7 @@ data_file(const char *pth, const char *fnm)
 	 /* BLANK LINE */
 	 skipline();
 	 process_eol();
-	 while (!feof(file.fh)) {
+	 while (ch != EOF) {
 	    if (ch == '\x0c') {
 	       nextch();
 	       process_eol();
@@ -722,7 +723,7 @@ data_file(const char *pth, const char *fnm)
       }
    } else if (fmt == FMT_MAK) {
       nextch_handling_eol();
-      while (!feof(file.fh) && !ferror(file.fh)) {
+      while (ch != EOF && !ferror(file.fh)) {
 	 if (ch == '#') {
 	    /* include a file */
 	    int ch_store;
@@ -807,7 +808,7 @@ data_file(const char *pth, const char *fnm)
 	 pcs = pcsParent;
       }
    } else {
-      while (!feof(file.fh) && !ferror(file.fh)) {
+      while (ch != EOF && !ferror(file.fh)) {
 	 if (!process_non_data_line()) {
 	    f_export_ok = fFalse;
 	    switch (pcs->style) {

tests/Makefile.am
@@ -126,4 +126,5 @@ cslonglat.out cslonglat.svx\
 omitfixaroundsolve.out omitfixaroundsolve.svx\
 repeatreading.svx repeatreading.out repeatreading.pos\
 mixedeols.out mixedeols.svx\
-utf8bom.out utf8bom.svx
+utf8bom.out utf8bom.svx\
+nonewlineateof.out nonewlineateof.svx

tests/cavern.tst
@@ -69,7 +69,7 @@ testdir=`(cd "$testdir" && pwd)`
  skipafterbadomit passagebad badreadingdotplus badcalibrate calibrate_clino\
  badunits badbegin anonstn anonstnbad anonstnrev doubleinc reenterlots\
  cs csbad csbadsdfix cslonglat omitfixaroundsolve repeatreading\
- mixedeols utf8bom\
+ mixedeols utf8bom nonewlineateof\
 "}}
 
 # Test file stnsurvey3.svx missing: pos=fail # We exit before the error count.

tests/nonewlineateof.out
@@ -1,0 +1,21 @@
+
+Removing trailing traverses...
+
+Concatenating traverses...
+
+Simplifying network...
+
+Calculating network...
+
+Calculating traverses...
+
+Calculating trailing traverses...
+
+Calculating statistics...
+
+Survey contains 1 survey station, joined by 0 legs.
+There are 0 loops.
+Total length of survey legs =    0.00m (   0.00m adjusted)
+Total plan length of survey legs =    0.00m
+Total vertical length of survey legs =    0.00m
+   1 0-node.

tests/nonewlineateof.svx
@@ -1,0 +1,2 @@
+; pos=no warn=0
+*fix 1 reference 0 0 0