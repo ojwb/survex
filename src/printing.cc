ea93ada9a94ea9173e958983330439ac06f60589
Olly Betts <olly@survex.com>
2017-07-04 05:17:07 +1200

    Ignore view for export formats which work in 3D
    When the rotation and tilt controls are hidden in the export dialog
    we were still using their values to transform the data, so if you
    set them with for one export format which support them, then
    switched to an export format which doesn't, you'd get bogus
    coordinates in the exported file.
    
    Bug probably introduced in 1.2.27 by fixes for exports of rotated plans
    and tilted elevations.

--- src/printing.cc
+++ src/printing.cc
@@ -731,10 +731,18 @@ svxPrintDlg::OnExport(wxCommandEvent&) {
 
 	try {
 	    const wxString& export_fnm = dlg.GetPath();
+	    unsigned mask = format_info[format_idx];
+	    double rot, tilt;
+	    if (mask & EXPORT_3D) {
+		rot = 0.0;
+		tilt = -90.0;
+	    } else {
+		rot = m_layout.rot;
+		tilt = m_layout.tilt;
+	    }
 	    if (!Export(export_fnm, m_layout.title,
 			m_layout.datestamp, m_layout.datestamp_numeric, mainfrm,
-			m_layout.rot, m_layout.tilt,
-			m_layout.get_effective_show_mask(),
+			rot, tilt, m_layout.get_effective_show_mask(),
 			export_format(format_idx), input_projection.utf8_str(),
 			grid, text_height, marker_size, m_layout.Scale)) {
 		wxString m = wxString::Format(wmsg(/*Couldn’t write file “%s”*/402).c_str(),
