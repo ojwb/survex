02ba4c9b93199e05f34cf33b50c19f63d9383d74
Olly Betts <olly@survex.com>
2018-06-15 18:24:30 +1200

    survexport: hook up all possible export settings

--- src/survexport.cc
+++ src/survexport.cc
@@ -56,20 +56,38 @@ main(int argc, char **argv)
    show_mask |= STNS;
    show_mask |= LABELS;
    show_mask |= LEGS;
-   show_mask |= SURF;
 
    const int OPT_FMT_BASE = 20000;
+   enum {
+       OPT_SCALE = 0x100, OPT_BEARING, OPT_TILT, OPT_PLAN, OPT_ELEV,
+       OPT_LEGS, OPT_SURF, OPT_SPLAYS, OPT_CROSSES, OPT_LABELS, OPT_ENTS,
+       OPT_FIXES, OPT_EXPORTS, OPT_XSECT, OPT_WALLS, OPT_PASG,
+       OPT_CENTRED, OPT_FULL_COORDS
+   };
    static const struct option long_opts[] = {
 	/* const char *name; int has_arg (0 no_argument, 1 required, 2 options_*); int *flag; int val */
 	{"survey", required_argument, 0, 's'},
-	{"no-crosses", no_argument, 0, 'c'},
-	{"no-station-names", no_argument, 0, 'n'},
-	{"no-legs", no_argument, 0, 'l'},
+	{"scale", required_argument, 0, OPT_SCALE},
+	{"bearing", required_argument, 0, OPT_BEARING},
+	{"tilt", required_argument, 0, OPT_TILT},
+	{"plan", no_argument, 0, OPT_PLAN},
+	{"elevation", no_argument, 0, OPT_ELEV},
+	{"no-legs", no_argument, 0, OPT_LEGS},
+	{"no-surface-legs", no_argument, 0, OPT_SURF},
+	{"splays", no_argument, 0, OPT_SPLAYS},
+	{"no-crosses", no_argument, 0, OPT_CROSSES},
+	{"no-station-names", no_argument, 0, OPT_LABELS},
+	{"entrances", no_argument, 0, OPT_ENTS},
+	{"fixes", no_argument, 0, OPT_FIXES},
+	{"exports", no_argument, 0, OPT_EXPORTS},
+	{"cross-sections", no_argument, 0, OPT_XSECT},
+	{"walls", no_argument, 0, OPT_WALLS},
+	{"passages", no_argument, 0, OPT_PASG},
+	{"origin-in-centre", no_argument, 0, OPT_CENTRED},
+	{"full-coordinates", no_argument, 0, OPT_FULL_COORDS},
 	{"grid", optional_argument, 0, 'g'},
 	{"text-height", required_argument, 0, 't'},
 	{"marker-size", required_argument, 0, 'm'},
-	{"elevation", required_argument, 0, 'e'},
-	{"reduction", required_argument, 0, 'r'},
 	{"dxf", no_argument, 0, OPT_FMT_BASE + FMT_DXF},
 	{"eps", no_argument, 0, OPT_FMT_BASE + FMT_EPS},
 	{"gpx", no_argument, 0, OPT_FMT_BASE + FMT_GPX},
@@ -82,36 +100,54 @@ main(int argc, char **argv)
 	{"svg", no_argument, 0, OPT_FMT_BASE + FMT_SVG},
 	{"help", no_argument, 0, HLP_HELP},
 	{"version", no_argument, 0, HLP_VERSION},
+	// US spelling:
+	{"origin-in-center", no_argument, 0, OPT_CENTRED},
+	// Abbreviation:
+	{"full-coords", no_argument, 0, OPT_FULL_COORDS},
 	{0,0,0,0}
    };
 
-#define short_opts "s:cnlg::t:m:e:r:DSPV"
+#define short_opts "s:g::t:m:"
 
    static struct help_msg help[] = {
 	/*			<-- */
 	{HLP_ENCODELONG(0),   /*only load the sub-survey with this prefix*/199, 0},
-	{HLP_ENCODELONG(1),   /*do not generate station markers*/100, 0},
-	{HLP_ENCODELONG(2),   /*do not generate station labels*/101, 0},
-	{HLP_ENCODELONG(3),   /*do not generate survey legs*/102, 0},
-	{HLP_ENCODELONG(4),   /*generate grid (default %sm)*/148, STRING(DEFAULT_GRID_SPACING)},
-	{HLP_ENCODELONG(5),   /*station labels text height (default %s)*/149, STRING(DEFAULT_TEXT_HEIGHT)},
-	{HLP_ENCODELONG(6),   /*station marker size (default %s)*/152, STRING(DEFAULT_MARKER_SIZE)},
-	{HLP_ENCODELONG(7),   /*produce an elevation view*/103, 0},
-	{HLP_ENCODELONG(8),   /*factor to scale down by (default %s)*/155, "500"},
-	{HLP_ENCODELONG(9),   /*produce DXF output*/156, 0},
-	{HLP_ENCODELONG(10),  /*produce EPS output*/454, 0},
-	{HLP_ENCODELONG(11),  /*produce GPX output*/455, 0},
-	{HLP_ENCODELONG(12),  /*produce HPGL output*/456, 0},
-	{HLP_ENCODELONG(13),  /*produce JSON output*/457, 0},
-	{HLP_ENCODELONG(14),  /*produce KML output*/458, 0},
+	{HLP_ENCODELONG(1),   /*scale (50, 0.02, 1:50 and 2:100 all mean 1:50)*/217, 0},
+	{HLP_ENCODELONG(2),   /*bearing (90, 90d, 100g all mean 90°)*/460, 0},
+	{HLP_ENCODELONG(3),   /*tilt (45, 45d, 50g, 100% all mean 45°)*/461, 0},
+	{HLP_ENCODELONG(4),   /*plan view (equivalent to --tilt=-90)*/462, 0},
+	{HLP_ENCODELONG(5),   /*elevation view (equivalent to --tilt=0)*/463, 0},
+	{HLP_ENCODELONG(6),   /*do not generate survey legs*/102, 0},
+	{HLP_ENCODELONG(7),   /*surface survey legs*/464, 0},
+	{HLP_ENCODELONG(8),   /*splay legs*/465, 0},
+	{HLP_ENCODELONG(9),   /*do not generate station markers*/100, 0},
+	{HLP_ENCODELONG(10),  /*do not generate station labels*/101, 0},
+	{HLP_ENCODELONG(11),  /*entrances*/466, 0},
+	{HLP_ENCODELONG(12),  /*fixed points*/467, 0},
+	{HLP_ENCODELONG(13),  /*exported stations*/468, 0},
+	{HLP_ENCODELONG(14),  /*cross-sections*/469, 0},
+	{HLP_ENCODELONG(15),  /*walls*/470, 0},
+	{HLP_ENCODELONG(16),  /*passages*/471, 0},
+	{HLP_ENCODELONG(17),  /*origin in centre*/472, 0},
+	{HLP_ENCODELONG(18),  /*full coordinates*/473, 0},
+
+	{HLP_ENCODELONG(19),  /*generate grid (default %sm)*/148, STRING(DEFAULT_GRID_SPACING)},
+	{HLP_ENCODELONG(20),  /*station labels text height (default %s)*/149, STRING(DEFAULT_TEXT_HEIGHT)},
+	{HLP_ENCODELONG(21),  /*station marker size (default %s)*/152, STRING(DEFAULT_MARKER_SIZE)},
+	{HLP_ENCODELONG(22),  /*produce DXF output*/156, 0},
+	{HLP_ENCODELONG(23),  /*produce EPS output*/454, 0},
+	{HLP_ENCODELONG(24),  /*produce GPX output*/455, 0},
+	{HLP_ENCODELONG(25),  /*produce HPGL output*/456, 0},
+	{HLP_ENCODELONG(26),  /*produce JSON output*/457, 0},
+	{HLP_ENCODELONG(27),  /*produce KML output*/458, 0},
 	/* TRANSLATORS: "Compass" and "Carto" are the names of software packages,
 	 * so should not be translated. */
-	{HLP_ENCODELONG(15),  /*produce Compass PLT output for Carto*/159, 0},
+	{HLP_ENCODELONG(28),  /*produce Compass PLT output for Carto*/159, 0},
 	/* TRANSLATORS: "Skencil" is the name of a software package, so should not be
 	 * translated. */
-	{HLP_ENCODELONG(16),  /*produce Skencil output*/158, 0},
-	{HLP_ENCODELONG(17),  /*produce Survex POS output*/459, 0},
-	{HLP_ENCODELONG(18),  /*produce SVG output*/160, 0},
+	{HLP_ENCODELONG(29),  /*produce Skencil output*/158, 0},
+	{HLP_ENCODELONG(30),  /*produce Survex POS output*/459, 0},
+	{HLP_ENCODELONG(31),  /*produce SVG output*/160, 0},
 	{0, 0, 0}
    };
 
@@ -122,17 +158,44 @@ main(int argc, char **argv)
       int opt = cmdline_getopt();
       if (opt == EOF) break;
       switch (opt) {
-       case 'e': /* Elevation */
-	 tilt = cmdline_double_arg();
+       case OPT_LEGS:
+	 show_mask &= ~LEGS;
 	 break;
-       case 'c': /* Crosses */
+       case OPT_SURF:
+	 show_mask |= SURF;
+	 break;
+       case OPT_SPLAYS:
+	 show_mask |= SPLAYS;
+	 break;
+       case OPT_CROSSES:
 	 show_mask &= ~STNS;
 	 break;
-       case 'n': /* Labels */
+       case OPT_LABELS:
 	 show_mask &= ~LABELS;
 	 break;
-       case 'l': /* Legs */
-	 show_mask &= ~LEGS;
+       case OPT_ENTS:
+	 show_mask |= SPLAYS;
+	 break;
+       case OPT_FIXES:
+	 show_mask |= FIXES;
+	 break;
+       case OPT_EXPORTS:
+	 show_mask |= EXPORTS;
+	 break;
+       case OPT_XSECT:
+	 show_mask |= XSECT;
+	 break;
+       case OPT_WALLS:
+	 show_mask |= WALLS;
+	 break;
+       case OPT_PASG:
+	 show_mask |= PASG;
+	 break;
+       case OPT_CENTRED:
+	 show_mask |= CENTRED;
+	 break;
+       case OPT_FULL_COORDS:
+	 show_mask |= FULL_COORDS;
 	 break;
        case 'g': /* Grid */
 	 if (optarg) {
@@ -141,8 +204,74 @@ main(int argc, char **argv)
 	    grid = (double)DEFAULT_GRID_SPACING;
 	 }
 	 break;
-       case 'r': /* Reduction factor */
-	 scale = cmdline_double_arg();
+       case OPT_SCALE: {
+	 char* colon = strchr(optarg, ':');
+	 if (!colon) {
+	     /* --scale=1000 => 1:1000 => scale = 1000 */
+	     scale = cmdline_double_arg();
+	     if (scale < 1.0) {
+		 /* --scale=0.001 => 1:1000 => scale = 1000 */
+		 scale = 1.0 / scale;
+	     }
+	 } else if (colon - optarg == 1 && optarg[0] == '1') {
+	     /* --scale=1:1000 => 1:1000 => scale = 1000 */
+	     optarg += 2;
+	     scale = cmdline_double_arg();
+	 } else {
+	     /* --scale=2:1000 => 1:500 => scale = 500 */
+	     *colon = '\0';
+	     scale = cmdline_double_arg();
+	     optarg = colon + 1;
+	     scale = cmdline_double_arg() / scale;
+	 }
+	 break;
+       }
+       case OPT_BEARING: {
+	 int units = 0;
+	 size_t len = strlen(optarg);
+	 if (len > 0) {
+	     char ch = optarg[len - 1];
+	     switch (ch) {
+		 case 'd':
+		 case 'g':
+		     units = ch;
+		     optarg[len - 1] = '\0';
+		     break;
+	     }
+	 }
+	 pan = cmdline_double_arg();
+	 if (units == 'g') {
+	     pan *= 0.9;
+	 }
+	 break;
+       }
+       case OPT_TILT: {
+	 int units = 0;
+	 size_t len = strlen(optarg);
+	 if (len > 0) {
+	     char ch = optarg[len - 1];
+	     switch (ch) {
+		 case '%':
+		 case 'd':
+		 case 'g':
+		     units = ch;
+		     optarg[len - 1] = '\0';
+		     break;
+	     }
+	 }
+	 tilt = cmdline_double_arg();
+	 if (units == 'g') {
+	     tilt *= 0.9;
+	 } else if (units == '%') {
+	     tilt = deg(atan(tilt * 0.01));
+	 }
+	 break;
+       }
+       case OPT_PLAN:
+	 tilt = -90.0;
+	 break;
+       case OPT_ELEV:
+	 tilt = 0.0;
 	 break;
        case 't': /* Text height */
 	 text_height = cmdline_double_arg();
