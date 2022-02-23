ojwb/survex

commit cccc545e42412c10d490e43a00555fb9ece3c271

Adapt proj_factors() patch from proj 8.2.0
This allows us to support proj >= 7.2.0 (older versions lack
proj_crs_get_datum_ensemble() which this patch needs).

src/commands.c
@@ -27,6 +27,11 @@
 #include <string.h>
 
 #include <proj.h>
+#if PROJ_VERSION_MAJOR < 8 || \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 2)
+// Needed for proj_factors workaround
+# include <proj_experimental.h>
+#endif
 
 #include "cavern.h"
 #include "commands.h"
@@ -1753,6 +1758,45 @@ cmd_declination(void)
 	    PJ_COORD lp;
 	    lp.lp.lam = lon;
 	    lp.lp.phi = lat;
+#if PROJ_VERSION_MAJOR < 8 || \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 2)
+	    // Code adapted from fix in PROJ 8.2.0 to make proj_factors() work in
+	    // cases we need (e.g. a CRS specified as "EPSG:<number>").
+	    switch (proj_get_type(pj)) {
+		case PJ_TYPE_PROJECTED_CRS: {
+		    // If it is a projected CRS, then compute the factors on the conversion
+		    // associated to it. We need to start from a temporary geographic CRS
+		    // using the same datum as the one of the projected CRS, and with
+		    // input coordinates being in longitude, latitude order in radian,
+		    // to be consistent with the expectations of the lp input parameter.
+
+		    PJ * geodetic_crs = proj_get_source_crs(PJ_DEFAULT_CTX, pj);
+		    if (!geodetic_crs)
+			break;
+		    PJ * datum = proj_crs_get_datum(PJ_DEFAULT_CTX, geodetic_crs);
+		    PJ * datum_ensemble = proj_crs_get_datum_ensemble(PJ_DEFAULT_CTX, geodetic_crs);
+		    PJ * cs = proj_create_ellipsoidal_2D_cs(
+			PJ_DEFAULT_CTX, PJ_ELLPS2D_LONGITUDE_LATITUDE, "Radian", 1.0);
+		    PJ * temp = proj_create_geographic_crs_from_datum(
+			PJ_DEFAULT_CTX, "unnamed crs", datum ? datum : datum_ensemble,
+			cs);
+		    proj_destroy(datum);
+		    proj_destroy(datum_ensemble);
+		    proj_destroy(cs);
+		    proj_destroy(geodetic_crs);
+		    PJ * newOp = proj_create_crs_to_crs_from_pj(PJ_DEFAULT_CTX, temp, pj, NULL, NULL);
+		    proj_destroy(temp);
+		    if (!newOp) {
+			break;
+		    }
+		    proj_destroy(pj);
+		    pj = newOp;
+		    break;
+		}
+		default:
+		    break;
+	    }
+#endif
 	    PJ_FACTORS factors = proj_factors(pj, lp);
 	    pcs->convergence = factors.meridian_convergence;
 	    proj_destroy(pj);

configure.ac
@@ -201,11 +201,11 @@ AC_SUBST([LIBAV_LIBS])
 AC_SUBST([LIBAV_CFLAGS])
 
 dnl Check for PROJ.
-PKG_CHECK_MODULES([PROJ], [proj >= 8.2.0], [
+PKG_CHECK_MODULES([PROJ], [proj >= 7.2.0], [
 ], [
   dnl pkg-config support in proj seems quite new, so probe directly if not
-  dnl found.  proj_trans_bounds() was added in PROJ 8.2.0.
-  AC_CHECK_LIB([proj], [proj_trans_bounds], [
+  dnl found.  We need proj_crs_get_datum_ensemble() which was added in 7.2.0.
+  AC_CHECK_LIB([proj], [proj_crs_get_datum_ensemble], [
       PROJ_LIBS=-lproj
       PROJ_CFLAGS=
     ], [

doc/HACKING.htm
@@ -49,7 +49,7 @@ been used successfully).
 <p>The wxWidgets library is used for aven's UI.  Currently >= 3.0.0 is
 supported.
 
-<p>The PROJ library is used for coordinate conversions.  Currently >= 8.2.0 is
+<p>The PROJ library is used for coordinate conversions.  Currently >= 7.2.0 is
 supported.
 
 <P>The Perl Locale::PO module is used for process message translation files.

tests/Makefile.am
@@ -123,8 +123,8 @@ cmd_prefix_bad.out cmd_prefix_bad.svx\
 doubleinc.out doubleinc.svx\
 reenterlots.out reenterlots.svx\
 cs.out cs.pos cs.svx\
-csbad.out csbad.svx\
-csbadsdfix.out csbadsdfix.svx\
+csbad.altout csbad.out csbad.svx\
+csbadsdfix.altout csbadsdfix.out csbadsdfix.svx\
 csfeet.out csfeet.pos csfeet.svx\
 cslonglat.out cslonglat.svx\
 omitfixaroundsolve.out omitfixaroundsolve.svx\

tests/cavern.tst
@@ -176,6 +176,7 @@ for file in $TESTS ; do
     input="./$file.svx" ;;
   esac
   outfile=$basefile.out
+  outfile2=$basefile.altout
   posfile=$basefile.pos
   rm -f tmp.*
   pwd=`pwd`
@@ -286,11 +287,18 @@ for file in $TESTS ; do
   esac
 
   if test -f "$outfile" ; then
-    # Check output is as expected, working around Apple's stone-age sed.
-    if test -n "$VERBOSE" ; then
-      sed '1,/^Copyright/d;/^\(CPU \)*[Tt]ime used  *[0-9][0-9.]*s$/d;s!.*/src/\(cavern: \)!\1!' tmp.out|diff "$outfile" - || exit 1
+    # Version and time used info from output, working around Apple's stone-age
+    # sed.
+    sed '1,/^Copyright/d;/^\(CPU \)*[Tt]ime used  *[0-9][0-9.]*s$/d;s!.*/src/\(cavern: \)!\1!' tmp.out > tmp.out2
+    mv tmp.out2 tmp.out
+    # Check output is as expected.
+    if cmp -s "$outfile" tmp.out ; then
+      : # Matches.
+    elif [ -f "$outfile2" ] && cmp -s "$outfile2" tmp.out ; then
+      : # Matches alternative output (e.g. due to older PROJ).
     else
-      sed '1,/^Copyright/d;/^\(CPU \)*[Tt]ime used  *[0-9][0-9.]*s$/d;s!.*/src/\(cavern: \)!\1!' tmp.out|cmp -s "$outfile" - || exit 1
+      test -z "$VERBOSE" || diff "$outfile" tmp.out
+      exit 1
     fi
   fi
   rm -f tmp.*

tests/csbad.altout
@@ -1,0 +1,98 @@
+./csbad.svx:2: error: Station "1" fixed before CS command first used
+./csbad.svx:3:5: error: Unknown coordinate system
+ *cs EPSG:-1
+     ^~~~~~~
+./csbad.svx:4:5: error: Unknown coordinate system
+ *cs ERSI:1234
+     ^~~~~~~~~
+./csbad.svx:5:5: error: Unknown coordinate system
+ *cs EUR79Z31
+     ^~~~~~~~
+./csbad.svx:6:11: error: End of line not blank
+ *cs IJTSK 03
+           ^
+./csbad.svx:7:5: error: Unknown coordinate system
+ *cs IJTSK04
+     ^~~~~~~
+./csbad.svx:8:5: error: Unknown coordinate system
+ *cs IJTSK99
+     ^~~~~~~
+./csbad.svx:9:5: error: Unknown coordinate system
+ *cs IJTSK3
+     ^~~~~~
+./csbad.svx:10:5: error: Unknown coordinate system
+ *cs IJTSK032
+     ^~~~~~~~
+./csbad.svx:11:5: error: Unknown coordinate system
+ *cs IJTSK03a
+     ^~~~~~~~
+./csbad.svx:12:5: error: Unknown coordinate system
+ *cs lat-lat
+     ^~~~~~~
+./csbad.svx:13:5: error: Unknown coordinate system
+ *cs Long-LONG
+     ^~~~~~~~~
+./csbad.svx:14:5: error: Unknown coordinate system
+ *cs OSGB:ZZ
+     ^~~~~~~
+./csbad.svx:15:5: error: Unknown coordinate system
+ *cs OSGB:HI
+     ^~~~~~~
+./csbad.svx:16:5: error: Unknown coordinate system
+ *cs S-LONG
+     ^~~~~~
+./csbad.svx:17:5: error: Unknown coordinate system
+ *cs UTM0
+     ^~~~
+./csbad.svx:18:5: error: Unknown coordinate system
+ *cs UTM61N
+     ^~~~~~
+./csbad.svx:19:5: error: Unknown coordinate system
+ *cs UTM61S
+     ^~~~~~
+./csbad.svx:20:12: error: Invalid coordinate system: unknown projection id
+ *cs custom "+proj=ection"
+            ^~~~~~~~~~~~~~
+./csbad.svx:21:9: error: Unknown coordinate system
+ *cs out lat-long
+         ^~~~~~~~
+./csbad.svx:22:9: error: Coordinate system unsuitable for output
+ *cs out long-lat
+         ^~~~~~~~
+./csbad.svx:23:9: error: Coordinate system unsuitable for output
+ *cs out jtsk
+         ^~~~
+./csbad.svx:24:9: error: Coordinate system unsuitable for output
+ *cs out jtsk03
+         ^~~~~~
+./csbad.svx:25:9: error: Coordinate system unsuitable for output
+ *cs out EPSG:4326
+         ^~~~~~~~~
+./csbad.svx:26:9: error: Coordinate system unsuitable for output
+ *cs out ESRI:104305
+         ^~~~~~~~~~~
+./csbad.svx:34: error: Failed to convert coordinates: latitude or longitude exceeded limits
+ *fix 2 reference 179 -89 1000
+
+Removing trailing traverses...
+
+Concatenating traverses...
+
+Simplifying network...
+
+Calculating network...
+
+Calculating traverses...
+
+Calculating trailing traverses...
+
+Calculating statistics...
+
+Survey contains 2 survey stations, joined by 0 legs.
+There are 0 loops.
+Survey has 2 connected components.
+Total length of survey legs =    0.00m (   0.00m adjusted)
+Total plan length of survey legs =    0.00m
+Total vertical length of survey legs =    0.00m
+   2 0-nodes.
+There were 0 warning(s) and 26 error(s) - no output files produced.

tests/csbadsdfix.altout
@@ -1,0 +1,89 @@
+./csbadsdfix.svx:2: error: Station "1" fixed before CS command first used
+./csbadsdfix.svx:3:5: error: Unknown coordinate system
+ *cs EPSG:-1
+     ^~~~~~~
+./csbadsdfix.svx:4:5: error: Unknown coordinate system
+ *cs ERSI:1234
+     ^~~~~~~~~
+./csbadsdfix.svx:5:5: error: Unknown coordinate system
+ *cs EUR79Z31
+     ^~~~~~~~
+./csbadsdfix.svx:6:11: error: End of line not blank
+ *cs IJTSK 03
+           ^
+./csbadsdfix.svx:7:5: error: Unknown coordinate system
+ *cs IJTSK04
+     ^~~~~~~
+./csbadsdfix.svx:8:5: error: Unknown coordinate system
+ *cs IJTSK99
+     ^~~~~~~
+./csbadsdfix.svx:9:5: error: Unknown coordinate system
+ *cs IJTSK3
+     ^~~~~~
+./csbadsdfix.svx:10:5: error: Unknown coordinate system
+ *cs IJTSK032
+     ^~~~~~~~
+./csbadsdfix.svx:11:5: error: Unknown coordinate system
+ *cs IJTSK03a
+     ^~~~~~~~
+./csbadsdfix.svx:12:5: error: Unknown coordinate system
+ *cs lat-lat
+     ^~~~~~~
+./csbadsdfix.svx:13:5: error: Unknown coordinate system
+ *cs Long-LONG
+     ^~~~~~~~~
+./csbadsdfix.svx:14:5: error: Unknown coordinate system
+ *cs OSGB:ZZ
+     ^~~~~~~
+./csbadsdfix.svx:15:5: error: Unknown coordinate system
+ *cs OSGB:HI
+     ^~~~~~~
+./csbadsdfix.svx:16:5: error: Unknown coordinate system
+ *cs S-LONG
+     ^~~~~~
+./csbadsdfix.svx:17:5: error: Unknown coordinate system
+ *cs UTM0
+     ^~~~
+./csbadsdfix.svx:18:5: error: Unknown coordinate system
+ *cs UTM61N
+     ^~~~~~
+./csbadsdfix.svx:19:5: error: Unknown coordinate system
+ *cs UTM61S
+     ^~~~~~
+./csbadsdfix.svx:20:12: error: Invalid coordinate system: unknown projection id
+ *cs custom "+proj=ection"
+            ^~~~~~~~~~~~~~
+./csbadsdfix.svx:21:9: error: Unknown coordinate system
+ *cs out lat-long
+         ^~~~~~~~
+./csbadsdfix.svx:22:9: error: Coordinate system unsuitable for output
+ *cs out long-lat
+         ^~~~~~~~
+./csbadsdfix.svx:23:9: error: Coordinate system unsuitable for output
+ *cs out jtsk
+         ^~~~
+./csbadsdfix.svx:24:9: error: Coordinate system unsuitable for output
+ *cs out jtsk03
+         ^~~~~~
+
+Removing trailing traverses...
+
+Concatenating traverses...
+
+Simplifying network...
+
+Calculating network...
+
+Calculating traverses...
+
+Calculating trailing traverses...
+
+Calculating statistics...
+
+Survey contains 1 survey station, joined by 0 legs.
+There are 0 loops.
+Total length of survey legs =    0.00m (   0.00m adjusted)
+Total plan length of survey legs =    0.00m
+Total vertical length of survey legs =    0.00m
+   1 0-node.
+There were 0 warning(s) and 23 error(s) - no output files produced.

commit fa63080d786fa15e1c2a54705123a7911c126093

Avoid segfault with PROJ < 8.1.0
Looks like this was introduced by
cccc545e42412c10d490e43a00555fb9ece3c271 so 1.4.0 and 1.4.1 only
full work on PROJ >= 8.1.0 (but the problem is only triggered if
"*declination auto" is used).

src/commands.c
@@ -1772,7 +1772,17 @@ cmd_declination(void)
 	/* Invalidate cached declination. */
 	pcs->declination = HUGE_REAL;
 	{
-	    PJ *pj = proj_create(PJ_DEFAULT_CTX, proj_str_out);
+	    // PJ_DEFAULT_CTX is really just NULL, but PROJ < 8.1.0
+	    // dereferences the context without a NULL check inside
+	    // proj_create_ellipsoidal_2D_cs() so create a context
+	    // temporarily to avoid a segmentation fault.
+	    PJ_CONTEXT * ctx = PJ_DEFAULT_CTX;
+#if PROJ_VERSION_MAJOR < 8 || \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 1)
+	    ctx = proj_context_create();
+#endif
+
+	    PJ *pj = proj_create(ctx, proj_str_out);
 	    PJ_COORD lp;
 	    lp.lp.lam = lon;
 	    lp.lp.phi = lat;
@@ -1790,10 +1800,10 @@ cmd_declination(void)
 		     * to be consistent with the expectations of the lp input parameter.
 		     */
 
-		    PJ * geodetic_crs = proj_get_source_crs(PJ_DEFAULT_CTX, pj);
+		    PJ * geodetic_crs = proj_get_source_crs(ctx, pj);
 		    if (!geodetic_crs)
 			break;
-		    PJ * datum = proj_crs_get_datum(PJ_DEFAULT_CTX, geodetic_crs);
+		    PJ * datum = proj_crs_get_datum(ctx, geodetic_crs);
 #if PROJ_VERSION_MAJOR == 8 || \
     (PROJ_VERSION_MAJOR == 7 && PROJ_VERSION_MINOR >= 2)
 		    /* PROJ 7.2.0 upgraded to EPSG 10.x which added the concept
@@ -1805,23 +1815,22 @@ cmd_declination(void)
 		     * them.
 		     */
 		    if (!datum) {
-			datum = proj_crs_get_datum_ensemble(PJ_DEFAULT_CTX, geodetic_crs);
+			datum = proj_crs_get_datum_ensemble(ctx, geodetic_crs);
 		    }
 #endif
 		    PJ * cs = proj_create_ellipsoidal_2D_cs(
-			PJ_DEFAULT_CTX, PJ_ELLPS2D_LONGITUDE_LATITUDE, "Radian", 1.0);
+			ctx, PJ_ELLPS2D_LONGITUDE_LATITUDE, "Radian", 1.0);
 		    PJ * temp = proj_create_geographic_crs_from_datum(
-			PJ_DEFAULT_CTX, "unnamed crs", datum, cs);
+			ctx, "unnamed crs", datum, cs);
 		    proj_destroy(datum);
 		    proj_destroy(cs);
 		    proj_destroy(geodetic_crs);
-		    PJ * newOp = proj_create_crs_to_crs_from_pj(PJ_DEFAULT_CTX, temp, pj, NULL, NULL);
+		    PJ * newOp = proj_create_crs_to_crs_from_pj(ctx, temp, pj, NULL, NULL);
 		    proj_destroy(temp);
-		    if (!newOp) {
-			break;
+		    if (newOp) {
+			proj_destroy(pj);
+			pj = newOp;
 		    }
-		    proj_destroy(pj);
-		    pj = newOp;
 		    break;
 		}
 		default:
@@ -1831,6 +1840,10 @@ cmd_declination(void)
 	    PJ_FACTORS factors = proj_factors(pj, lp);
 	    pcs->convergence = factors.meridian_convergence;
 	    proj_destroy(pj);
+#if PROJ_VERSION_MAJOR < 8 || \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 1)
+	    proj_context_destroy(ctx);
+#endif
 	}
     } else {
 	/* *declination D UNITS */