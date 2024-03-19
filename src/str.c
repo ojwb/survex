2ca70032671ecadeb0c7e201d2e5ead779f96575
Olly Betts <olly@survex.com>
2024-03-20 10:09:55 +1300

    Fix append of empty string to unallocated dynamic string

--- src/str.c
+++ src/str.c
@@ -33,7 +33,7 @@ void s_expand_(string *pstr, int addition) {
 void
 s_catlen(string* pstr, const char *s, int s_len)
 {
-   if (pstr->capacity - pstr->len < s_len)
+   if (pstr->capacity - pstr->len < s_len || s_len == 0)
        s_expand_(pstr, s_len);
    memcpy(pstr->s + pstr->len, s, s_len);
    pstr->len += s_len;
@@ -43,7 +43,7 @@ s_catlen(string* pstr, const char *s, int s_len)
 void
 s_catn(string *pstr, int n, char c)
 {
-   if (pstr->capacity - pstr->len < n)
+   if (pstr->capacity - pstr->len < n || n == 0)
        s_expand_(pstr, n);
    memset(pstr->s + pstr->len, c, n);
    pstr->len += n;
